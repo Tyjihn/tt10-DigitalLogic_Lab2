bd68a39f46b18090e45ed4a16586fdf7b2b34bfe
Tyjihn <149103628+Tyjihn@users.noreply.github.com>
2025-02-24 19:44:09 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_DigitalLogic_Lab2 user_project (
+ tt_um_DigitalLogic_Lab2 user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
